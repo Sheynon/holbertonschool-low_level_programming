fd7737b23079024ee1d4b59b7f975a3b1ce88f07
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-17 11:22:07 +0100

    script to print a specific phrase

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print "With proper grammar, but the outcome is a piece of art,"
+ * using the printf fonction.
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
