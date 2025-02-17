28ab8d276ee970c4fa6283265b6248f268740309
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-17 15:08:22 +0100

    print a specific phrase

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ * Description: Prints "and that piece of art is useful\"
+ * - Dora Korpar, 2015-10-19"
+ *
+ * Return: Always return 1 (error)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
