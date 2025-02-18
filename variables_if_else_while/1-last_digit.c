3c5eae0120241aec06b569c5154fb3ccc96db77b
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-18 15:59:15 +0100

    Print if the last digit of the random number is 0, between 0 and 6 or 6 and more.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 
 	printf("last digit of %d is %d ", n, last_digit);
 
-	if (last_digit < 5)
+	if (last_digit > 5)
 		printf("and is greater than 5\n");
 	else if (last_digit == 0)
 		printf("and is 0\n");
