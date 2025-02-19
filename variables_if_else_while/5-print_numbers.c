0d9992dd68c1d296d23c6906980e9b0ba674a15f
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-19 09:03:21 +0100

    Print all single digit number from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print all single digit number from 0 to 10.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		printf("%d", b);
+	}
+	printf("\n");
+	return (0);
+}
