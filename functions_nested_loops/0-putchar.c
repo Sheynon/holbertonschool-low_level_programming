a70720a5cb409ac5e7ad937bb9bb55179927af15
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-19 15:57:27 +0100

    correction : print _putchar without printf or any function from the standart

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,15 +11,7 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	write(1, "_putchar\n", 9);
 	return (0);
 }
 
