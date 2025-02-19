477e4ca32b8739b284169b670100ffb15fa56b67
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-19 15:08:23 +0100

    correcting directory name

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%d\n", _putchar);
+	return (0);
+}
+
