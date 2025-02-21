f78b32c2c6ce6cbf20f85a27325b42c978c2d53b
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-21 14:45:28 +0100

    update - function to print the tables from 0 to 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - Function that prints the 9 table
+ *
+ * Description: Print the 9 table starting from 0
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			_putchar(c + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		_putchar('\n');
+	}
+}
+
