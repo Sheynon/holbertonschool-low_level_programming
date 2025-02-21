4691f6c7edd7b96629eca42685dcff41118351d7
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-21 13:09:19 +0100

    function to return the absolute value of an integrer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Function that compute the absolute value of an integrer
+ *
+ * Description: return the absolute value of the integrer
+ *
+ * Return: -a if the if is true a if it's false
+ */
+
+int _abs(int)
+{
+	int a;
+
+	if (a < 0)
+		return (-a);
+	return (a);
+}
