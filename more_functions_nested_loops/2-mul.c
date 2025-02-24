b49a0309db62fc0c8b74323b04851d81d1bd888d
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-24 09:46:32 +0100

    Function to multiplicate two integrer

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Function to execute the program
+ * @a: First integrer to use in the operation
+ * @b: Second integrer to use in the operation
+ *
+ * Description: Function to multiplicate two integrer
+ *
+ * Return: Return the result of the operation
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
