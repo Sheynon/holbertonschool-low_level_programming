9596d2364e183136ddd0d12101863c500082e430
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-24 10:18:43 +0100

    function to print more numbers (0 to 14)

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function to execute in the program
+ *
+ * Description: Function to print numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 14; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
