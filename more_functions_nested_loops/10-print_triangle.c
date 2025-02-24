d54c3f90fec5593a1a3efcfa21bf2d3651af30ab
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-24 12:43:13 +0100

    function to print a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,9 +15,15 @@ void print_triangle(int size)
 		_putchar('\n');
 
 	for (a = 0; a < size; a++)
+	{
 		for (b = 0; b < size - a - 1; b++)
+		{
 			_putchar(' ');
+		}
 		for (b = 0; b <= a; b++)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
+	}
 }
