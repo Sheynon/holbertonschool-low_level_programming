8199dd9984a9d8d60f9204dd133cda77492ca824
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-24 12:10:15 +0100

    print numbre from 1 to 100, replace multiple of 3 by Fizz multiple of 5 by Buzz multiple of both by FIzzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,22 +8,25 @@
  * Print Buzz for multiple of 5
  * Print FizzBuzz for both
  * Separate each number by a space
+ *
+ * return: Always 0 (success)
  */
 
-void main(void)
+int main(void)
 {
 	int a, b;
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if ((a % 3 == 0) && (a % 5 == 0))
+			printf("FizzBuzz ");
+		else if (a % 3 == 0)
 			printf("Fizz ");
 		else if (a % 5 == 0)
 			printf("Buzz ");
-		else if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz ");
 		else
 			printf("%d ", a);
 	}
 	printf("\n");
+	return (0);
 }
