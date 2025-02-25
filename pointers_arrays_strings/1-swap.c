cbe82267ed68a29886be2bdb117a1bd7c11fa845
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-25 13:22:22 +0100

    function to swap the value of two integrer

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Function to execute in the program
+ * @a: first integrer
+ * @b: second integrer
+ *
+ * Description: Function to swap the value of two integrer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = b;
+}
