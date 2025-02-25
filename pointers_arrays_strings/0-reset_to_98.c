cd86906607829219a5907e76c1258040242664a7
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-25 12:50:50 +0100

    function to take a pointer int as parameter and update it's value to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Entry point of the program
- * @*n: pointer n to work with
+ * @n: pointer n to work with
  *
  * Description: take the pointer of a int parameter and updates
  * the value to 98
