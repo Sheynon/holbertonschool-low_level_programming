2ffe37fcc301f2616e787870a30e7673051f207b
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-26 11:01:34 +0100

    function to copy a string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * *_strcpy - function to execute in the program
+ * _strcpy - function to execute in the program
  * @dest: first integrer
  * @src: second integrer
  *
  * Description: function to copy a string pointed by "src"
  * including the terminating null byte ("\0")
  * to the buffer pointed by dest
+ *
+ * Return: return the value of the integrer "dest"
  */
 
 char *_strcpy(char *dest, char *src)
@@ -22,5 +24,5 @@ char *_strcpy(char *dest, char *src)
 
 	dest[a] = '\0';
 
-	return dest;
+	return (dest);
 }
