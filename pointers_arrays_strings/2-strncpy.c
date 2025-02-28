568ac94f326494c5f522f028aa9e17b665fb2196
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-28 15:44:41 +0100

    function that copy a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (src[n] != '\0')
+	int a = 0;
+
+	while (a < n && src[a] != '\0')
 	{
-		dest[n] = src[n];
-		n++;
+		dest[a] = src[a];
+		a++;
 	}
-	dest[n] = '\0';
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
 
 	return (dest);
 }
