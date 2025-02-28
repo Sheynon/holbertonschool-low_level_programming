a9946b9e4d06e5bb2dd22288511de7741ec92e9b
Sheynon <labadie.jonatahan@hotmail.fr>
2025-02-28 11:05:56 +0100

    function that concatenate a specific amount of bytes from the source to the destination

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *strncat - Function to execute in the program
+ * @dest: Destination integrer
+ * @src: Source Integrer
+ * @n: Third Integrer
+ *
+ * Description: Function that concatenate two string for a
+ * specific amount of bytes from the source
+ *
+ * Return: return the content of "dest"
+ */
+
+char *strncar(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (b < n && src[b] !='\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
