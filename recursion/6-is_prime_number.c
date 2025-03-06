1a1695689296e042e8613e211db84b8a8c068af6
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-07 00:57:43 +0100

    function that return 1 if the integrer is a prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * is_prime_recursive - Function to execute
+ * @n: First Intgrer
+ * @i: Second Integrer
+ *
+ * Description: Function that verify a number in a recursive
+ *
+ * Return: 1 if it's not a prime number 0 if it is
+ */
+
+int is_prime_recursive(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	else if (i * i > n)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+
+	return (is_prime_recursive(n, i + 1));
+}
+/**
  * is_prime_number - Function to execute in the code
  * @n: First integrer
  *
@@ -13,14 +34,5 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-		return (0);
-	else if (i * i > n)
-		return (1);
-	else if (n % i == 0)
-		return (0);
-
-	return (check_prime(n, i + 1));
+	return (is_prime_recursive(n, 2));
 }
