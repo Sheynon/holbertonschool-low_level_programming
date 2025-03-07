48d0360f1c2787f2ad5caf05047d6224c22b92d3
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-07 11:18:58 +0100

    program to show the soze of the array argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Inntegrer
+ * @argv: array of value
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
