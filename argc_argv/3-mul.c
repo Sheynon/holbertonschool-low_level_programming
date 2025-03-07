193277687232a12eb8c6cc767cf952c0fbb09528
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-07 13:24:51 +0100

    Program to multiple 2 number from an array

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Integrer
+ * @argv: array of value
+ *
+ * Return: 0 for sucess 1 for faillure
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, sum;
+
+	(void)argc;
+	if (argv[1] == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	sum = num1 * num2;
+
+	printf("%d\n", sum);
+	return (0);
+
+}
