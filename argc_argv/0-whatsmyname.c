6faa812c9409555166eed28ae3480077762bd103
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-07 11:12:24 +0100

    program to write it's own name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Integrer
+ * @argv: Array value
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
