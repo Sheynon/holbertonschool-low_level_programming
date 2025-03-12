fafac019be8f5c9f3a7da2a86731a088d376255d
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-12 22:02:53 +0100

    correction for concatenate two string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len_s2)
 		len_s2 = n;
 
-	result = malloc(sizeof(char) * (len_s1 + n + 1);
+	result = malloc(sizeof(char) * (len_s1 + n + 1));
 
 	if (result == NULL)
 		return (NULL);
