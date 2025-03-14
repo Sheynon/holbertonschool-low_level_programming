c40787dbcbcb45d94418eb525c32a6e308eca316
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-14 19:31:03 +0100

    correction to dog comment for the sturture project

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure to represent a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
