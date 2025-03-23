c8e2431cd9480c1c09735fcf28f892ddc3ab2939
Sheynon <labadie.jonatahan@hotmail.fr>
2025-03-23 14:31:05 +0100

    fixed - betty syntaxe

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,23 +11,20 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	char *str;
-
 	va_list args;
 
 	va_start(args, format);
-
 	while (format == NULL)
 	{
 		printf("\n");
 		return;
 	}
-
 	while (format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", (char) va_arg(args,int));
+				printf("%c", (char) va_arg(args, int));
 				break;
 			case 'i':
 				printf("%d", va_arg(args, int));
@@ -38,23 +35,16 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(args, char *);
 				if (str != NULL)
-				{
 					printf("%s", str);
 					break;
-				}
-				else
-				{
-					printf("(nil)");
-					break;
-				}
+				printf("(nil)");
+				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'f' ||
-			format[i] == 's' || format[i] == 'i') &&
-				format[(i + 1)] != '\0')
+		if ((format[i] == 'c' || format[i] == 'f' || format[i] == 's' ||
+			format[i] == 'i') && format[(i + 1)] != '\0')
 			printf(", ");
 		i++;
 	}
-
 	va_end(args);
 	printf("\n");
 }
