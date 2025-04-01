29e569ee9e6ae3e18d0886d291a439d5e22a6310
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-01 17:03:04 +0200

    fixed - error in the function causing in to not compile because new_node wasn't initiated

--- single_linked_lists/2-add_node.c
+++ single_linked_lists/2-add_node.c
@@ -17,13 +17,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new_node)
 		return (NULL);
 
+	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len =strlen(str);
+	new_node->len = strlen(str);
 
 	new_node->next = *head;
 	*head = new_node;
