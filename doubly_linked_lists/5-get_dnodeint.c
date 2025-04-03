fba4b8597a60e9789c9dbe7dfd9856446b9ff54d
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-03 17:23:39 +0200

    fixed - order of operation to be done

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -15,10 +15,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head)
 	{
 		if (i == index)
-		{
 			return (head);
-			head = head->next;
-		}
+
+		head = head->next;
 		i++;
 	}
 	return (NULL);
