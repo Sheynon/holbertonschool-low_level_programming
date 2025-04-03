d851a0aa39388cef2318fb5c4e685684734e78d3
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-03 12:18:02 +0200

    fixed - incorect if loop to a while loop

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	dup_str = strdup(str);
-	if (!dup_str[len])
+	while (dup_str[len])
 		len++;
 
 	new_node = malloc(sizeof(list_t));
