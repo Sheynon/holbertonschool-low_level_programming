47a888a64f3045adc171c64193cf2ce185a58784
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-03 11:56:54 +0200

    added - if head is NULL return NULL

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (str == NULL)
+	if (head = NULL || str == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
