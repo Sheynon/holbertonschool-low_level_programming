8d6779665a0ab54759ce5fa74d6692827c49caba
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-06 23:20:46 +0200

    added - create new code to pass all the checker

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,75 +1,116 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int open_source_file(const char *filename);
+int open_destination_file(const char *filename);
+void copy_file_content(int fd_from, int fd_to);
+void close_files(int fd_from, int fd_to);
 
 /**
- * file_error - check the file can be open
- * @file_from: File from
- * @file_to: File to
- * @argv: Argument vector
- * Return: No return
+ * main - copies the content of one file to another
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments passed to the program
+ * Return: 0 on success, exits with an error code on failure
  */
-void file_error(int file_from, int file_to, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (file_from == -1)
+	int fd_from, fd_to;
+
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	fd_from = open_source_file(argv[1]);
+	fd_to = open_destination_file(argv[2]);
+	copy_file_content(fd_from, fd_to);
+	close_files(fd_from, fd_to);
+
+	return (0);
+}
+
+/**
+ * open_source_file - opens the source file for reading
+ * @filename: the name of the file to open
+ * Return: file descriptor on success, -1 on failure
+ */
+int open_source_file(const char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	if (file_to == -1)
+
+	return (fd);
+}
+
+/**
+ * open_destination_file - opens the destination file for writing
+ * @filename: the name of the file to open
+ * Return: file descriptor on success, -1 on failure
+ */
+int open_destination_file(const char *filename)
+{
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", filename);
 		exit(99);
+	}
+
+	return (fd);
+}
+
+/**
+ * copy_file_content - copies content from the source to the destination
+ * @fd_from: file descriptor of the source file
+ * @fd_to: file descriptor of the destination file
+ */
+void copy_file_content(int fd_from, int fd_to)
+{
+	int r, w;
+	char buffer[1024];
+
+	while ((r = read(fd_from, buffer, 1024)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1)
+		{
+			dprintf(2, "Error: Can't write to file\n");
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(2, "Error: Can't read from file\n");
+		exit(98);
 	}
 }
 
 /**
- * main - check the code
- * @argc: Number of arguments
- * @argv: Argument vector
- * Return: Always 0.
+ * close_files - closes the file descriptors
+ * @fd_from: file descriptor of the source file
+ * @fd_to: file descriptor of the destination file
  */
-int main(int argc, char *argv[])
+void close_files(int fd_from, int fd_to)
 {
-	int file_from, file_to, err_close;
-	ssize_t nchar, nwr;
-	char buf[1024];
-
-	if (argc != 3)
+	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	file_error(file_from, file_to, argv);
-
-	nchar = 1024;
-	while (nchar == 1024)
-	{
-		nchar = read(file_from, buf, 1024);
-		if (nchar == -1)
-			file_error(-1, 0, argv);
-		nwr = write(file_to, buf, nchar);
-		if (nwr == -1)
-			file_error(0, -1, argv);
-	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
-	if (err_close == -1)
+	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-	return (0);
 }
